2f9dadb6fbefb2b8247b373e3b2876420acce236
Josh Bosley <broskisworld@gmail.com>
2018-11-17 13:26:07 -0700

    ninja initial code

---
+++ lazyhero/src/ninja.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#include "LazyWorld.h"
+#include "GodController.h"
+#include "Entity.h"
+
+#define NINJA_STAMINA 1.0	//seconds
+#define NINJA_ATTACK_RECHARGE 1.0	//seconds
+#define NINJA_ATTACK_DAMAGE 10	//???
+
+typedef enum
+{
+	IDLE2,
+	CHASING,
+	ATTACK,
+	TELEPORT,
+	DIE
+}ninjaState;
+
+class ninja : public Entity
+{
+	ninjaState aiState;
+	animation idle, running, attack, die;
+public:
+	ninja();
+	void ai();
+	~ninja();
+};
